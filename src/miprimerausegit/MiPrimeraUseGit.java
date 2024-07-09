0660a01b1efbe27b33a6c92a63c2390df21da32a
Demi trujillo <diemiliango012@gmail.com>
2024-07-09 08:05:24 -0600

    se agrego otro ciclo

--- src/miprimerausegit/MiPrimeraUseGit.java
+++ src/miprimerausegit/MiPrimeraUseGit.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package miprimerausegit;
 
-/**
- *
- * @author HP
- */
+
 public class MiPrimeraUseGit {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hola mamaica");
+        }
+        System.out.println("esete proyecto lo realice con git");
+        System.out.println("hoy es 2 de julio");
     }
     
 }
